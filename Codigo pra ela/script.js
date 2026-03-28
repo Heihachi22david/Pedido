a5d9c611025fe80c2178f427e42f8da9f943bcfa
Heihachi22david <davidgamer24000@gmail.com>
2026-03-28 13:22:33 +0000

    Delete Codigo pra ela directory

--- Codigo pra ela/script.js
+++
@@ -1,9 +0,0 @@
-function aceitar(event) {
-    event.stopPropagation();
-    document.getElementById('check').style.display = 'inline';
-    
-    // Pequeno delay para ela ver o "check" antes de mudar de página
-    setTimeout(() => {
-        window.location.href = "TeAmo.html";
-    }, 800);
-}
